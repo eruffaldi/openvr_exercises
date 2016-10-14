d8b5fd3d7031d0cb7164445636c275d1639afcd5
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2016-10-14 14:57:36 +0200

    refactored and cleanup

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # openvr_exercises
 For the course IVE
+
+
+# Minimal OpenVR Notes
+
+## Matrices
+
+vr::HmdMatrix44_t mat = m_pHMD->GetProjectionMatrix( nEye, m_fNearClip, m_fFarClip, vr::API_OpenGL);
+vr::HmdMatrix43_t mat = m_pHMD->GetEyeToHeadTransform( nEye );
+
+	vr::VRCompositor()->WaitGetPoses(m_rTrackedDevicePose, vr::k_unMaxTrackedDeviceCount, NULL, 0 );
+	
---
+++ src/hellovr_base.h
@@ -0,0 +1,244 @@
+#pragma once
+#include "hellovr_model.h"
+#include <openvr.h>
+#include <SDL.h>
+#include <GL/glew.h>
+#include <SDL_opengl.h>
+#include <stdio.h>
+#include <string>
+#include <cstdlib>
+
+#include "shared/lodepng.h"
+#include "shared/Matrices.h"
+#include "shared/pathtools.h"
+
+
+#if defined(POSIX)
+#include "unistd.h"
+#endif
+#ifndef WIN32
+#define stricmp strcasecmp
+#define strnicmp strncasecmp
+#define sprintf_s snprintf
+#define vsprintf_s vsnprintf
+#define APIENTRY 
+#endif
+
+inline void ThreadSleep( unsigned long nMilliseconds )
+{
+#if defined(_WIN32)
+	::Sleep( nMilliseconds );
+#elif defined(POSIX)
+	usleep( nMilliseconds * 1000 );
+#endif
+}
+
+
+//-----------------------------------------------------------------------------
+// Purpose: Converts a SteamVR matrix to our local matrix class
+//-----------------------------------------------------------------------------
+inline Matrix4 convert( const vr::HmdMatrix34_t &matPose )
+{
+	return Matrix4(
+		matPose.m[0][0], matPose.m[1][0], matPose.m[2][0], 0.0,
+		matPose.m[0][1], matPose.m[1][1], matPose.m[2][1], 0.0,
+		matPose.m[0][2], matPose.m[1][2], matPose.m[2][2], 0.0,
+		matPose.m[0][3], matPose.m[1][3], matPose.m[2][3], 1.0f
+		);
+}
+
+inline Matrix4 convert( const vr::HmdMatrix44_t &mat )
+{
+	return Matrix4(
+		mat.m[0][0], mat.m[1][0], mat.m[2][0], mat.m[3][0],
+		mat.m[0][1], mat.m[1][1], mat.m[2][1], mat.m[3][1], 
+		mat.m[0][2], mat.m[1][2], mat.m[2][2], mat.m[3][2], 
+		mat.m[0][3], mat.m[1][3], mat.m[2][3], mat.m[3][3]
+	);
+}
+
+extern bool g_bPrintf;
+
+//-----------------------------------------------------------------------------
+// Purpose:
+//-----------------------------------------------------------------------------
+inline void dprintf( const char *fmt, ... )
+{
+	va_list args;
+	char buffer[ 2048 ];
+
+	va_start( args, fmt );
+#ifndef WIN32
+	vsnprintf(  buffer,sizeof(buffer),fmt,args);
+#else
+	vsprintf_s( buffer, fmt, args );
+#endif
+	va_end( args );
+
+	if ( g_bPrintf )
+		printf( "%s", buffer );
+
+#ifndef WIN32
+	puts(buffer);
+#else
+	OutputDebugStringA( buffer );
+#endif
+}
+//-----------------------------------------------------------------------------
+// Purpose:
+//------------------------------------------------------------------------------
+class CMainApplication
+{
+public:
+	CMainApplication( int argc, char *argv[] );
+	virtual ~CMainApplication();
+
+	bool BInit();
+	bool BInitGL();
+	bool BInitCompositor();
+
+	void SetupRenderModels();
+
+	void Shutdown();
+
+	void RunMainLoop();
+	bool HandleInput();
+	void ProcessVREvent( const vr::VREvent_t & event );
+	void RenderFrame();
+
+	bool SetupTexturemaps();
+
+	void SetupScene();
+	void AddCubeToScene( Matrix4 mat, std::vector<float> &vertdata );
+	void AddCubeVertex( float fl0, float fl1, float fl2, float fl3, float fl4, std::vector<float> &vertdata );
+
+	void DrawControllers();
+
+	bool SetupStereoRenderTargets();
+	void SetupDistortion();
+	void SetupCameras();
+
+	void RenderStereoTargets();
+	void RenderDistortion();
+	virtual void RenderScene( vr::Hmd_Eye nEye ) = 0;
+
+	Matrix4 GetHMDMatrixProjectionEye( vr::Hmd_Eye nEye );
+	Matrix4 GetHMDMatrixPoseEye( vr::Hmd_Eye nEye );
+	Matrix4 GetCurrentViewProjectionMatrix( vr::Hmd_Eye nEye );
+	void UpdateHMDMatrixPose();
+
+
+	GLuint CompileGLShader( const char *pchShaderName, const char *pchVertexShader, const char *pchFragmentShader );
+	bool CreateAllShaders();
+
+	void SetupRenderModelForTrackedDevice( vr::TrackedDeviceIndex_t unTrackedDeviceIndex );
+	CGLRenderModel *FindOrLoadRenderModel( const char *pchRenderModelName );
+
+protected: 
+	bool m_bDebugOpenGL;
+	bool m_bVerbose;
+	bool m_bPerf;
+	bool m_bVblank;
+	bool m_bGlFinishHack;
+
+	vr::IVRSystem *m_pHMD;
+	vr::IVRRenderModels *m_pRenderModels;
+	std::string m_strDriver;
+	std::string m_strDisplay;
+	vr::TrackedDevicePose_t m_rTrackedDevicePose[ vr::k_unMaxTrackedDeviceCount ];
+	Matrix4 m_rmat4DevicePose[ vr::k_unMaxTrackedDeviceCount ];
+	bool m_rbShowTrackedDevice[ vr::k_unMaxTrackedDeviceCount ];
+
+protected: // SDL bookkeeping
+	SDL_Window *m_pWindow;
+	uint32_t m_nWindowWidth;
+	uint32_t m_nWindowHeight;
+
+	SDL_GLContext m_pContext;
+
+protected: // OpenGL bookkeeping
+	int m_iTrackedControllerCount;
+	int m_iTrackedControllerCount_Last;
+	int m_iValidPoseCount;
+	int m_iValidPoseCount_Last;
+	bool m_bShowCubes;
+
+	std::string m_strPoseClasses;                            // what classes we saw poses for this frame
+	char m_rDevClassChar[ vr::k_unMaxTrackedDeviceCount ];   // for each device, a character representing its class
+
+	int m_iSceneVolumeWidth;
+	int m_iSceneVolumeHeight;
+	int m_iSceneVolumeDepth;
+	float m_fScaleSpacing;
+	float m_fScale;
+	
+	int m_iSceneVolumeInit;                                  // if you want something other than the default 20x20x20
+	
+	float m_fNearClip;
+	float m_fFarClip;
+
+	GLuint m_iTexture;
+
+	unsigned int m_uiVertcount;
+
+	GLuint m_glSceneVertBuffer;
+	GLuint m_unSceneVAO;
+	GLuint m_unLensVAO;
+	GLuint m_glIDVertBuffer;
+	GLuint m_glIDIndexBuffer;
+	unsigned int m_uiIndexSize;
+
+	GLuint m_glControllerVertBuffer;
+	GLuint m_unControllerVAO;
+	unsigned int m_uiControllerVertcount;
+
+	Matrix4 m_mat4HMDPose;
+	Matrix4 m_mat4eyePosLeft;
+	Matrix4 m_mat4eyePosRight;
+
+	Matrix4 m_mat4ProjectionCenter;
+	Matrix4 m_mat4ProjectionLeft;
+	Matrix4 m_mat4ProjectionRight;
+
+	struct VertexDataScene
+	{
+		Vector3 position;
+		Vector2 texCoord;
+	};
+
+	struct VertexDataLens
+	{
+		Vector2 position;
+		Vector2 texCoordRed;
+		Vector2 texCoordGreen;
+		Vector2 texCoordBlue;
+	};
+
+	GLuint m_unSceneProgramID;
+	GLuint m_unLensProgramID;
+	GLuint m_unControllerTransformProgramID;
+	GLuint m_unRenderModelProgramID;
+
+	GLint m_nSceneMatrixLocation;
+	GLint m_nControllerMatrixLocation;
+	GLint m_nRenderModelMatrixLocation;
+
+	struct FramebufferDesc
+	{
+		GLuint m_nDepthBufferId;
+		GLuint m_nRenderTextureId;
+		GLuint m_nRenderFramebufferId;
+		GLuint m_nResolveTextureId;
+		GLuint m_nResolveFramebufferId;
+	};
+	FramebufferDesc leftEyeDesc;
+	FramebufferDesc rightEyeDesc;
+
+	bool CreateFrameBuffer( int nWidth, int nHeight, FramebufferDesc &framebufferDesc );
+	
+	uint32_t m_nRenderWidth;
+	uint32_t m_nRenderHeight;
+
+	std::vector< CGLRenderModel * > m_vecRenderModels;
+	CGLRenderModel *m_rTrackedDeviceToRenderModel[ vr::k_unMaxTrackedDeviceCount ];
+};
